a82128a0463fa66fa8cf8ed8150cf6a0371a117c
vuetim <vuetimvue@gmail.com>
2023-12-22 09:49:18 -0600

    logo issue

--- woonuxt_base/components/generalElements/Logo.vue
+++ woonuxt_base/components/generalElements/Logo.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 const runtimeConfig = useRuntimeConfig();
 const logoUrl = runtimeConfig?.public?.LOGO || null;
+const isProduction = process.env.NODE_ENV === 'production';
 </script>
 
 <template>
   <NuxtLink to="/">
     <img v-if="logoUrl" :src="logoUrl" alt="Logo" class="object-contain h-10" />
     <div v-else class="flex items-center gap-4 w-[280] text-lg font-bold">
-      <NuxtImg src="images/logoqaf.png" width="400" height="80" />
+      <NuxtImg :src="isProduction ? '/_vercel/image?url=/images/logoqaf.png' : '/images/logoqaf.png'" width="400" height="80" />
     </div>
   </NuxtLink>
 </template>
